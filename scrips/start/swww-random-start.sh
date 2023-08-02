5453fba688d5cc3a1750c0706911db22daef4029
CoolerWuffi <96253562+CoolerWuffi@users.noreply.github.com>
2023-08-02 02:36:00 +0000

    Delete scrips directory
    Part 1: Fixed a misspelling (dont know how to rename a dir)

--- scrips/start/swww-random-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR=~/.config/backgrounds/
-PICS=($(ls ${DIR}))
-
-sleep 2
-RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
-swww img ${DIR}/${RANDOMPICS} --transition-fps 60 --transition-type any --transition-duration 3
