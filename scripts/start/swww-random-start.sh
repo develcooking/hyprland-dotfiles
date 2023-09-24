81525890e0aec0d3353f1dc30840cd6a0f7976a6
CoolerWuffi <96253562+CoolerWuffi@users.noreply.github.com>
2023-09-24 21:04:05 +0200

    Delete scripts/start/swww-random-start.sh

--- scripts/start/swww-random-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-swww init
-DIR=~/.config/backgrounds/
-PICS=($(ls ${DIR}))
-
-sleep 6
-RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
-swww img ${DIR}/${RANDOMPICS} --transition-fps 60 --transition-type any --transition-duration 3
