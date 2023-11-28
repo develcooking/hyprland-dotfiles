8d98682feda2632070c1f13b46ca79d2b0ead265
CoolerWuffi <96253562+CoolerWuffi@users.noreply.github.com>
2023-11-28 19:02:18 +0000

    added flatpak support

--- scripts/start/darktheme-at-start.sh
+++ scripts/start/darktheme-at-start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+#set adwaita theme in qt apps
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
 
-gsettings set org.gnome.desktop.interface gtk-theme adwaita-dark
 gsettings set org.gnome.desktop.wm.preferences theme adwaita-dark
 
+#flatpak 
+sudo flatpak override --env GTK_THEME=Adwaita-dark
 
-
-#gtk 4
+#set dark mode as prefered default
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+#set adwaita dark as gtk theme
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
-
